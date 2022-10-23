c150b411f08defb0d8f89077fd389ef9b3b60985
zZ7_aki <zakariae.bouklah18@gmail.com>
2022-10-23 18:22:12 +0100

    Temporary UI && switching between activities

--- app/src/main/java/com/example/note_app/MainActivity.java
+++ app/src/main/java/com/example/note_app/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.note_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void launchLogin(View v)
+    {
+        Intent i = new Intent(this, LoginPage.class);
+        startActivity(i);
+    }
+
+    public void launchRegistration(View v)
+    {
+        Intent i = new Intent(this, RegistrationPage.class);
+        startActivity(i);
     }
 }
---
+++ app/src/main/java/com/example/note_app/RegistrationPage.java
@@ -0,0 +1,15 @@
+package com.example.note_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegistrationPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration_page);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+}
